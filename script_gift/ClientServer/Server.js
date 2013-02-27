cd9d8822e8bff042799d20115de8a5144ead9157
Christopher Burke <chris@krslynx.com>
2013-02-27 12:26:29 +0000

    Added an example Client and Server

---
+++ script_gift/ClientServer/Server.js
@@ -0,0 +1,34 @@
+var Http = require('http');
+var Util = require('util');
+
+var Server = function() {
+	
+	var launchTime = 0;
+	var requestCount = 0;
+	this.httpServer_ = Http.createServer(function(req, res) {
+		var responseData = 'Hello, client.\n';
+		requestCount+=1;
+		res.write(responseData);
+		res.end();
+	});
+	this.httpServer_.listen(8080, function() {
+		console.log("Server Listening on port: " + 8080);
+		launchTime = Date.now() / 1000;
+	});
+	
+	var lastLogged = 0;
+	
+	var perSecond = function() {
+		if(requestCount != 0) {
+			console.log('Serving ' + requestCount + ' requests/s');
+		}
+		setTimeout(perSecond, 1000);
+		requestCount = 0;
+	};
+	
+	setTimeout(perSecond(), 1000);
+};
+
+new Server();
+
+exports.Server = Server;
